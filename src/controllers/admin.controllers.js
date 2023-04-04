8fac49841bd498cefa310d3897528671d614dd92
Julián Viera <julianviera2019@hotmail.com>
2023-04-04 11:44:14 -0300

    Julián - Función orden entregada terminada

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -1,4 +1,4 @@
-const { Product, User, CartProduct, Cart, Order } = require("../database/models/index");
+const { Product, User, Order } = require("../database/models/index");
 
 module.exports = {
     stats: async (req, res) => {
@@ -8,14 +8,14 @@ module.exports = {
             const ordersDB = await Order.findAll()
             const productsDB = await Product.findAll()
             const usersDB = await User.findAll()
-    
+
             ordersDB.forEach(order => {
                 totalSales = totalSales + parseFloat(order.total)
                 if (order.delivered) {
                     quantityOrdesDelivered = quantityOrdesDelivered + 1
                 }
             });
-    
+
             const data = {
                 totalSales,
                 quantityOrdesDelivered,
@@ -23,8 +23,19 @@ module.exports = {
                 quantityProducts: productsDB.length,
                 quantityUsers: usersDB.length
             }
-    
+
             return res.status(200).json(data);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    },
+    delivered: async (req, res) => {
+        try {
+            const orderDB = await Order.findByPk(req.body.orderId)
+
+            await orderDB.update({ delivered: true })
+
+            return res.status(200).json("Order updated");
         } catch (error) {
             return res.status(500).json(error);
         }
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,5 +3,6 @@ const router = Router();
 const adminControllers = require("../controllers/admin.controllers")
 
 router.get("/stats", adminControllers.stats)
+router.post("/orders/delivered", adminControllers.delivered)
 
 module.exports = router
