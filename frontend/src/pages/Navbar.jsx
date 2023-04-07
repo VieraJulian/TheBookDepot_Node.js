cc96332b652b4956053a1eb5d041e33d23d35173
Julián Viera <julianviera2019@hotmail.com>
2023-04-07 18:23:06 -0300

    Julián - Navbar terminado

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,59 +1,73 @@
 import { Link } from 'react-router-dom'
 
 import "../../public/css/navbar-mobile.css"
+import "../../public/css/navbar-desktop.css"
 
 function Navbar() {
     return (
         <>
-            <div className='navbar-container'>
-                <div className='navbar-top'>
-                    <Link to="#">
-                        <p className='navbar-p-bd'>TheBookDepot</p>
-                    </Link>
-                    <Link to="#">
-                        <p className='navbar-p-mc'>Mi Cuenta</p>
-                    </Link>
-                </div>
-                <picture className='navbar-logo'>
-                    <Link to="/">
-                        <img src="../../public/img/logo.png" alt="Logo" />
-                    </Link>
-                </picture>
-                <div className='navbar-center'>
-                    <Link to="#" className='link-bars'>
-                        <div className='bars'>
-                            <i class="fa-solid fa-bars-staggered"></i>
-                        </div>
-                    </Link>
-                    <div className='navbar-search'>
-                        <input type="text" 
-                        placeholder='Buscar por título...'
-                        spellCheck="false"
-                        autoComplete="off"/>
-                        <button>Buscar</button>
-                    </div>
-                    <div className='cart'>
+            <div className='navbar'>
+                <div className='navbar-container'>
+                    <div className='navbar-top'>
                         <Link to="#">
-                            <i class="fa-solid fa-cart-shopping"></i>
+                            <p className='navbar-p-bd'>TheBookDepot</p>
                         </Link>
-                        <div className='cart-number'>
-                            0
+                        <Link to="#">
+                            <p className='navbar-p-mc'>Mi Cuenta</p>
+                        </Link>
+                    </div>
+                    <picture className='navbar-logo'>
+                        <Link to="/">
+                            <img src="../../public/img/logo.png" alt="Logo" />
+                        </Link>
+                    </picture>
+                    <div className='navbar-center'>
+                        <Link to="#" className='link-bars'>
+                            <div className='bars'>
+                                <i class="fa-solid fa-bars-staggered"></i>
+                            </div>
+                        </Link>
+                        <div className='navbar-search'>
+                            <input type="text"
+                                placeholder='Buscar por título...'
+                                spellCheck="false"
+                                autoComplete="off" />
+                            <button>Buscar</button>
+                        </div>
+                        <div className='cart-detail'>
+                            <div className='cart-product'>
+                                <p className='cart-quantity'>0</p>
+                                <p>producto</p>
+                            </div>
+                            <p className='p-bar'>|</p>
+                            <div className='cart-price'>
+                                <p>AR$</p>
+                                <p className='cart-total'>0.00</p>
+                            </div>
+                        </div>
+                        <div className='cart'>
+                            <Link to="#">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </Link>
+                            <div className='cart-number'>
+                                0
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='navbar-bottom'>
-                    <Link to="#">
-                        <p>Libros</p>
-                    </Link>
-                    <Link to="#">
-                        <p>Más Vendidos</p>
-                    </Link>
-                    <Link to="#">
-                        <p>Español</p>
-                    </Link>
-                    <Link to="#">
-                        <p>Inglés</p>
-                    </Link>
+                    <div className='navbar-bottom'>
+                        <Link to="#">
+                            <p>Libros</p>
+                        </Link>
+                        <Link to="#">
+                            <p>Más Vendidos</p>
+                        </Link>
+                        <Link to="#">
+                            <p>Español</p>
+                        </Link>
+                        <Link to="#">
+                            <p>Inglés</p>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
