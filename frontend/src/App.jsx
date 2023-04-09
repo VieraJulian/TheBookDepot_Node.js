4f73db6dc1a0e86ca601b6b8aa32cf776baf23d2
Julián Viera <julianviera2019@hotmail.com>
2023-04-09 14:58:19 -0300

    Julián - Footer terminado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Navbar from "./pages/Navbar"
 import Home from "./pages/Home"
+import Footer from "./pages/Footer"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
+      <Footer /> 
     </>
   )
 }
---
+++ frontend/src/pages/Footer.jsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom'
+
+import "../../public/css/footer-mobile.css"
+
+function Footer() {
+    return (
+        <>
+            <div className='footer'>
+                <div className='footer-container'>
+                    <picture>
+                        <img src="../../public/img/logo.png" alt="Logo" />
+                    </picture>
+                    <nav className='footer-links'>
+                        <Link to="#">
+                            <p>Inicio</p>
+                        </Link>
+                        <Link to="#">
+                            <p>Libros</p>
+                        </Link>
+                        <Link to="#">
+                            <p>Mangas</p>
+                        </Link>
+                        <Link to="#">
+                            <p>Más vendidos</p>
+                        </Link>
+                        <Link to="#">
+                            <p>Inglés</p>
+                        </Link>
+                    </nav>
+                    <div className='copy-container'>
+                        <p>© 2023 The Book Depot. Todos los derechos reservados.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
