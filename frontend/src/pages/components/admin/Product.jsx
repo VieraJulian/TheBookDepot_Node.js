f087ef479fc9b13e482478f1dccd843994614f06
Julián Viera <julianviera2019@hotmail.com>
2023-04-24 12:57:58 -0300

    Julián - Trabajando en la vista de productos para el administrador

--- frontend/src/pages/components/admin/Product.jsx
+++ frontend/src/pages/components/admin/Product.jsx
@@ -1,8 +1,12 @@
 import "../../../../public/css/components-admin/product/product-mobile.css"
+import "../../../../public/css/components-admin/product/product-desktop.css"
 
 function Product() {
     return (
         <>
+            <div className="msg-mobile">
+                <p>"Esta página puede no funcionar correctamente en dispositivos móviles o tablets."</p>
+            </div>
             <div className="product-container">
                 <button className="add-product">AGREGAR NUEVO PRODUCTO</button>
                 <div className="products-order-container">
@@ -15,32 +19,61 @@ function Product() {
                         <button><i class="fa-solid fa-caret-right"></i></button>
                     </div>
                     <div className="detail-product">
-                        <div>
-                            <p>Imagen</p>
-                            <p>Nombre</p>
-                            <p>Precio</p>
-                            <p>Stock</p>
-                            <p>Vendidos</p>
-                            <p>Detalle</p>
-                        </div>
-                        <div>
-                            <div>
-                                <p>Imagen</p>
-                                <p>Nombre</p>
-                                <p>Precio</p>
-                                <p>Stock</p>
-                                <p>Vendidos</p>
-                                <p>Detalle</p>
+                        <div className="info-product-container">
+                            <div className="info-product-header">
+                                <p className="p-imagen">Imagen</p>
+                                <p className="p-nombre">Nombre</p>
+                                <p className="p-precio">Precio</p>
+                                <p className="p-stock">Stock</p>
+                                <p className="p-vendido">Vendidos</p>
+                                <p className="p-detalle">Detalle</p>
                             </div>
-                            <div>
+                            <div className="info-product">
                                 <div className="product-detail-img">
                                     <img src="../../../../public/img/balondeoro.jpg" alt="" />
                                 </div>
-                                <p>Lorem ipsum dolor sit.</p>
-                                <p>$ 5000.00</p>
-                                <p>234</p>
-                                <p>190</p>
-                                <button><i class="fa-solid fa-eye"></i></button>
+                                <p className="p-info-nombre">Lorem ipsum dolor sit.</p>
+                                <p className="p-info-detail">$5000.00</p>
+                                <p className="p-info-detail">234</p>
+                                <p className="p-info-detail">190</p>
+                                <div className="view-detail">
+                                    <button><i class="fa-solid fa-eye"></i></button>
+                                </div>
+                            </div>
+                            <div className="info-product">
+                                <div className="product-detail-img">
+                                    <img src="../../../../public/img/balondeoro.jpg" alt="" />
+                                </div>
+                                <p className="p-info-nombre">Lorem ipsum dolor sit.</p>
+                                <p className="p-info-detail">$99000.00</p>
+                                <p className="p-info-detail">234</p>
+                                <p className="p-info-detail">190</p>
+                                <div className="view-detail">
+                                    <button><i class="fa-solid fa-eye"></i></button>
+                                </div>
+                            </div>
+                            <div className="info-product">
+                                <div className="product-detail-img">
+                                    <img src="../../../../public/img/balondeoro.jpg" alt="" />
+                                </div>
+                                <p className="p-info-nombre">Lorem ipsum dolor sit.</p>
+                                <p className="p-info-detail">$5000.00</p>
+                                <p className="p-info-detail">234</p>
+                                <p className="p-info-detail">190</p>
+                                <div className="view-detail">
+                                    <button><i class="fa-solid fa-eye"></i></button>
+                                </div>
+                            </div><div className="info-product">
+                                <div className="product-detail-img">
+                                    <img src="../../../../public/img/balondeoro.jpg" alt="" />
+                                </div>
+                                <p className="p-info-nombre">Lorem ipsum dolor sit.</p>
+                                <p className="p-info-detail">$5000.00</p>
+                                <p className="p-info-detail">234</p>
+                                <p className="p-info-detail">190</p>
+                                <div className="view-detail">
+                                    <button><i class="fa-solid fa-eye"></i></button>
+                                </div>
                             </div>
                         </div>
                     </div>
