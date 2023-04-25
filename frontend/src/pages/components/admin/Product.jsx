a25ff0027dff13e11883a966748ecdc2a74c61f2
Julián Viera <julianviera2019@hotmail.com>
2023-04-25 12:49:17 -0300

    Julián - Modal crear producto terminado

--- frontend/src/pages/components/admin/Product.jsx
+++ frontend/src/pages/components/admin/Product.jsx
@@ -1,3 +1,6 @@
+import { React } from "react"
+import Modal from "../modal/Modal"
+
 import "../../../../public/css/components/admin/product/product-mobile.css"
 import "../../../../public/css/components/admin/product/product-desktop.css"
 
@@ -8,15 +11,16 @@ function Product() {
                 <p>"Esta página puede no funcionar correctamente en dispositivos móviles o tablets."</p>
             </div>
             <div className="product-container">
-                <button className="add-product">AGREGAR NUEVO PRODUCTO</button>
+                <button className="add-product" data-bs-toggle="modal" data-bs-target="#modal">AGREGAR NUEVO PRODUCTO</button>
+                <Modal />
                 <div className="products-order-container">
                     <div className="pagination-container">
-                        <button><i class="fa-solid fa-caret-left"></i></button>
+                        <button><i className="fa-solid fa-caret-left"></i></button>
                         <button>1</button>
                         <button>2</button>
                         <button>3</button>
                         <button>4</button>
-                        <button><i class="fa-solid fa-caret-right"></i></button>
+                        <button><i className="fa-solid fa-caret-right"></i></button>
                     </div>
                     <div className="detail-product">
                         <div className="info-product-container">
@@ -37,7 +41,7 @@ function Product() {
                                 <p className="p-info-detail">234</p>
                                 <p className="p-info-detail">190</p>
                                 <div className="view-detail">
-                                    <button><i class="fa-solid fa-eye"></i></button>
+                                    <button><i className="fa-solid fa-eye"></i></button>
                                 </div>
                             </div>
                             <div className="info-product">
@@ -49,7 +53,7 @@ function Product() {
                                 <p className="p-info-detail">234</p>
                                 <p className="p-info-detail">190</p>
                                 <div className="view-detail">
-                                    <button><i class="fa-solid fa-eye"></i></button>
+                                    <button><i className="fa-solid fa-eye"></i></button>
                                 </div>
                             </div>
                             <div className="info-product">
@@ -61,7 +65,7 @@ function Product() {
                                 <p className="p-info-detail">234</p>
                                 <p className="p-info-detail">190</p>
                                 <div className="view-detail">
-                                    <button><i class="fa-solid fa-eye"></i></button>
+                                    <button><i className="fa-solid fa-eye"></i></button>
                                 </div>
                             </div><div className="info-product">
                                 <div className="product-detail-img">
@@ -72,7 +76,7 @@ function Product() {
                                 <p className="p-info-detail">234</p>
                                 <p className="p-info-detail">190</p>
                                 <div className="view-detail">
-                                    <button><i class="fa-solid fa-eye"></i></button>
+                                    <button><i className="fa-solid fa-eye"></i></button>
                                 </div>
                             </div>
                         </div>
---
+++ frontend/src/pages/components/modal/Modal.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+import "../../../../public/css/modal/modal.css"
+
+function Modal() {
+    return (
+        <>
+            <div className="modal fade" id="modal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Agregar nuevo producto</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form method="POST" className="form-modal">
+                                <label>Título</label>
+                                <input type="text" name="title" />
+                                <label>Autor</label>
+                                <input type="text" name="author" />
+                                <label>Editorial</label>
+                                <input type="text" name="editorial" />
+                                <label>Precio</label>
+                                <input type="text" name="price" />
+                                <label>Collección</label>
+                                <input type="text" name="collection" />
+                                <label>Números de páginas</label>
+                                <input type="number" name="numberPages" />
+                                <label>Idioma</label>
+                                <input type="text" name="language" />
+                                <label>Formato</label>
+                                <input type="text" name="format" />
+                                <label>ISBN</label>
+                                <input type="text" name="isbn" />
+                                <label>Peso en gramos</label>
+                                <input type="number" name="weight" />
+                                <label>Edición</label>
+                                <input type="number" name="edition" />
+                                <label>Stock</label>
+                                <input type="number" name="stock" />
+                                <label>Imagen</label>
+                                <input type="file" name="image" className="file-modal"/>
+                                <div class="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    <button type="button" className="btn btn-primary">Aceptar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Modal
